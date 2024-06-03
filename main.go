637179bb2588ffc76ff9ec65a81f6a1327d9f9e8
vinicius bosso <104734169+vinyhi@users.noreply.github.com>
2024-06-03 10:26:56 +0200

    error catching update

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"github.com/joho/godotenv"
 )
 
 func init() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file: %s", err)
+		log.Printf("Warning: Error loading .env file: %s", err)
 	}
 }
 
@@ -35,6 +35,10 @@ func connectDB() *mongo.Client {
 		log.Fatalf("Failed to connect to MongoDB: %s", err)
 	}
 
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatalf("Failed to ping MongoDB: %s", err)
+	}
+
 	fmt.Println("Connected to MongoDB.")
 	return client
 }
@@ -43,7 +47,29 @@ func movieRoutes(router *gin.Engine, client *mongo.Client) {
 	moviesCollection := client.Database("movieDB").Collection("movies")
 
 	router.GET("/movies", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "List of movies"})
+		var movies []interface{}
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		cur, err := moviesCollection.Find(ctx, "{}")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch movies from database"})
+			log.Printf("Failed to fetch movies: %s", err)
+			return
+		}
+		defer cur.Close(ctx)
+
+		for cur.Next(ctx) {
+			var movie interface{}
+			err := cur.Decode(&movie)
+			if err != nil {
+				log.Printf("Error decoding movie: %s", err)
+				continue
+			}
+			movies = append(movies, movie)
+		}
+
+		c.JSON(http.StatusOK, gin.H{"movies": movies})
 	})
 }
 
