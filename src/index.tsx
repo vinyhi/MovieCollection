3c72475b358cb48aaf2d11b8eb0a171ca8b1e578
vinicius bosso <104734169+vinyhi@users.noreply.github.com>
2024-06-25 04:17:46 +0200

    improve code readability

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+const rootElement = document.getElementById('root');
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
