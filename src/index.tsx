c75cda9f15c9d935ac9d2eff1a4f81d951ae529f
vinicius bosso <104734169+vinyhi@users.noreply.github.com>
2024-06-26 06:14:19 +0200

    error handling update

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,48 @@
+import React, { Component, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export default class ErrorBoundary extends Component<Props, State> {
+  state: State = { hasError: false };
+
+  static getDerivedStateFromError(_: Error): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong.</h2>;
+    }
+
+    return this.props.children;
+  }
+}
+```
+
+```tsx
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import ErrorBoundary from './ErrorBoundary';
+
 const rootElement = document.getElementById('root');
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
   rootElement
 );
